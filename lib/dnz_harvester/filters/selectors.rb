a08171b5de316f28c8a07ce7c30584e78202ab49
Federico Gonzalez <fedegl@gmail.com>
2012-12-10 11:12:41 +1300

    Refactor Filters::Selector to avoid duplication in subclasses

--- lib/dnz_harvester/filters/selectors.rb
+++ lib/dnz_harvester/filters/selectors.rb
@@ -10,21 +10,27 @@ module DnzHarvester
       def contents(attribute_name)
         contents = *record.original_attributes[attribute_name.to_sym]
       end
+
+      def selected_values(attribute_name)
+        raise NotImplementedError.new("All subclasses of DnzHarvester::Filters::Selector must override #selected_values.")
+      end
+
+      def within(attribute_name)
+        values = selected_values(attribute_name)
+        values = values.first if values.is_a?(Array) && scope == :first
+        values
+      end
     end
 
     class WithSelector < Selector
-      def within(attribute_name)
-        result = contents(attribute_name).find_all {|c| c.to_s.match(regexp) }
-        result = result.first if scope == :first
-        result
+      def selected_values(attribute_name)
+        contents(attribute_name).find_all {|c| c.to_s.match(regexp) }
       end
     end
 
     class WithoutSelector < Selector
-      def within(attribute_name)
-        result = contents(attribute_name).reject {|c| c.to_s.match(regexp) }
-        result = result.first if scope == :first
-        result
+      def selected_values(attribute_name)
+        contents(attribute_name).reject {|c| c.to_s.match(regexp) }
       end
     end
   end
