6b257c8e0442d416f7398aa205b262df1bae0944
Andy Gray <andy.gray@boost.co.nz>
2013-04-26 14:21:46 +1200

    Fixed some nil class exceptions and increased the depth limit on broad_related

--- lib/harvester_core/enrichments/tapuhi_authorities_enrichment.rb
+++ lib/harvester_core/enrichments/tapuhi_authorities_enrichment.rb
@@ -20,16 +20,18 @@ module HarvesterCore
 
         while ancestor_tap = queued_ancestors.shift
           iteration_count += 1
-          raise "Iteration is too deep (#{iteration_count}) for record #{record.record_id}" if iteration_count >= 15
+          raise "Iteration is too deep (#{iteration_count}) for record #{record.record_id}" if iteration_count >= 50
           
           ancestor = find_record(ancestor_tap)
 
-          @attributes[:authorities] << {authority_id: ancestor.tap_id, name: "broad_related_authority", text: ancestor.title}
-          processed_ancestors << ancestor.tap_id
+          unless ancestor.nil?
+            @attributes[:authorities] << {authority_id: ancestor.tap_id, name: "broad_related_authority", text: ancestor.title}
+            processed_ancestors << ancestor.tap_id
 
-          queued_ancestors += ancestor.authority_taps(:broader_term)
-          queued_ancestors.uniq!
-          queued_ancestors -= (processed_ancestors+parents)
+            queued_ancestors += ancestor.authority_taps(:broader_term)
+            queued_ancestors.uniq!
+            queued_ancestors -= (processed_ancestors+parents)
+          end
         end
       end
     end
