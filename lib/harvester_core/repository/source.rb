4b42d6df8d93e62cbfa47565c95378808214ff88
Michael Treacher <michael.treacher@boost.co.nz>
2013-05-02 14:45:13 +1200

    Added in method for getting the relation field.

--- lib/harvester_core/repository/source.rb
+++ lib/harvester_core/repository/source.rb
@@ -5,5 +5,9 @@ module Repository
     embedded_in :record, class_name: "Repository::Record"
 
     embeds_many :authorities, cascade_callbacks: true, class_name: "Repository::Authority"
+
+    def relation
+    	self[:relation]
+    end
   end
 end
