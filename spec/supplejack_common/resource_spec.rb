810ab1871eaa5063f6137ad530f96ef20de42ec6
Reinkaos <richard@boost.co.nz>
2017-10-30 14:45:38 +1300

    Improve Rspec description

--- spec/supplejack_common/resource_spec.rb
+++ spec/supplejack_common/resource_spec.rb
@@ -15,7 +15,7 @@ describe SupplejackCommon::Resource do
   describe "#fetch_document" do
     let(:resource) { klass.new("http://google.com/1", {throttling_options: {host: "google.com", delay: 1}, http_headers: { 'x-api-key': 'key'} }) }
 
-    it "request the resource with the throttling options and request timeout" do
+    it "request the resource with the throttling options, request timeout, and http_headers" do
       SupplejackCommon::Request.should_receive(:get).with("http://google.com/1", 60000, {host: "google.com", delay: 1}, { 'x-api-key': 'key'} )
       resource.send(:fetch_document)
     end
