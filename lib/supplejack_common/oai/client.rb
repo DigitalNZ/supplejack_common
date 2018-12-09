1b4c1df42533e6978ca49228188254f9c3efac34
Reinkaos <richard@boost.co.nz>
2018-12-10 09:59:46 +1300

    Fix position of the get method

--- lib/supplejack_common/oai/client.rb
+++ lib/supplejack_common/oai/client.rb
@@ -10,39 +10,45 @@ module OAI
       @base = URI.parse base_url
       @debug = options.fetch(:debug, false)
       @parser = options.fetch(:parser, 'rexml')
+
       @http_client = options.fetch(:http) do
         Faraday.new(url: @base.clone, proxy: proxy) do |builder|
           follow_redirects = options.fetch(:redirects, true)
           if follow_redirects
             count = follow_redirects.is_a?(Integer) ? follow_redirects : 5
+
             require 'faraday_middleware'
             builder.response :follow_redirects, limit: count
           end
           builder.adapter :net_http
-          builder.request :retry, max: 5, interval: 1, interval_randomness: 0.5, backoff_factor: 2
+          builder.request :retry, max: 5,
+                                  interval: 1,
+                                  interval_randomness: 0.5,
+                                  backoff_factor: 2
         end
       end
-    end
 
-    def get(uri)
-      response = @http_client.get("?#{uri.query}")
-      response.body
-    end
 
-    # load appropriate parser
-    case @parser
-    when 'libxml'
-      begin
-        require 'rubygems'
-        require 'xml/libxml'
-      rescue StandardError
-        raise OAI::Exception, 'xml/libxml not available'
+      # load appropriate parser
+      case @parser
+      when 'libxml'
+        begin
+          require 'rubygems'
+          require 'xml/libxml'
+        rescue StandardError
+          raise OAI::Exception, 'xml/libxml not available'
+        end
+      when 'rexml'
+        require 'rexml/document'
+        require 'rexml/xpath'
+      else
+        raise OAI::Exception, "unknown parser: #{@parser}"
       end
-    when 'rexml'
-      require 'rexml/document'
-      require 'rexml/xpath'
-    else
-      raise OAI::Exception, "unknown parser: #{@parser}"
     end
+  end
+
+  def get(uri)
+    response = @http_client.get("?#{uri.query}")
+    response.body
   end
 end
