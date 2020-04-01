3ece4f43d2fb08677248b832746c4e785e92d63b
Dave Harris <dave@harris.org.nz>
2020-04-01 15:33:58 +1300

    Revert "[#171483511] Encode Pagination token"

--- spec/supplejack_common/request_spec.rb
+++ spec/supplejack_common/request_spec.rb
@@ -42,8 +42,8 @@ describe SupplejackCommon::Request do
     end
 
     it 'URI escapes the url' do
-      request = klass.new('google.com/ben ten+', nil)
-      request.url.should eq 'google.com/ben%20ten%2B'
+      request = klass.new('google.com/ben ten', nil)
+      request.url.should eq 'google.com/ben%20ten'
     end
 
     it 'can be initialized with headers' do
