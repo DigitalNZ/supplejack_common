bb6877eaadd95061157f876a3ff7888b42298e0d
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2024-04-10 17:43:27 +0200

    fix: fixed issues related to bundle update...
    `option_call` wasn't defined anymore
    Faraday was updated to v2 in the OAI gem, this created a bug. So I explicitely required faraday to avoid future confusion

--- lib/supplejack_common/validations/format.rb
+++ lib/supplejack_common/validations/format.rb
@@ -6,12 +6,10 @@ module ActiveModel
       def validate_each(record, attribute, value)
         value = Array(value)
 
-        if options[:with]
-          regexp = option_call(record, :with)
+        if (regexp = options[:with])
           matches = value.map { |v| v !~ regexp }
           record_error(record, attribute, :with, value) if matches.include?(true)
-        elsif options[:without]
-          regexp = option_call(record, :without)
+        elsif (regexp = options[:without])
           record_error(record, attribute, :without, value) if value.to_s =~ regexp
         end
       end
